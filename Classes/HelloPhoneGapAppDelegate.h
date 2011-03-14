8446935291dcc0ec105f7f1643121674aea423e1
hutley <hutley@chariotsolutions.com>
2011-03-14 14:25:18 -0400

    init

---
+++ Classes/HelloPhoneGapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  HelloPhoneGapAppDelegate.h
+//  HelloPhoneGap
+//
+//  Created by Hiedi Utley on 3/14/11.
+//  Copyright Chariot Solutions, LLC 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface HelloPhoneGapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloPhoneGap
+//
+//  Created by Hiedi Utley on 3/14/11.
+//  Copyright Chariot Solutions, LLC 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"HelloPhoneGapAppDelegate");
+    [pool release];
+    return retVal;
+}
